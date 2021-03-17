c5303e7ff3ba43b79140045870765f30e9ba0708
Erik M. Buck <erik.buck@sbcglobal.net>
2021-03-16 20:04:10 -0400

    PR#1 Incorporated review feedback: Made split script strings over multiple lines to make test cases more readable.

--- test/CommandControllerTest.cpp
+++ test/CommandControllerTest.cpp
@@ -19,24 +19,35 @@ TEST(CommandController, emptyModel) {
 
 TEST(CommandController, runScript0) {
     WSU::Model::CommandController commandController;
-    commandController.runScript("{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}");
+    commandController.runScript(
+    "{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}");
     EXPECT_EQ("a", commandController.getCurrentStoredStringValue());
 }
 
 TEST(CommandController, runScript1) {
     WSU::Model::CommandController commandController;
-    commandController.runScript("{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"character\": \"-\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}");
+    commandController.runScript(
+    "{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"character\": \"-\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}");
     EXPECT_EQ("-a", commandController.getCurrentStoredStringValue());
 }
 
 TEST(CommandController, runScript2) {
     WSU::Model::CommandController commandController;
-    commandController.runScript("{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"character\": \"-\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"character\": \"!\", \"command\": \"insertCharacterAtIndex\", \"index\": 1}");
+    commandController.runScript(
+    "{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"character\": \"-\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"character\": \"!\", \"command\": \"insertCharacterAtIndex\", \"index\": 1}");
     EXPECT_EQ("a!-a", commandController.getCurrentStoredStringValue());
 }
 
 TEST(CommandController, runScript3) {
     WSU::Model::CommandController commandController;
-    commandController.runScript("{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"character\": \"-\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}{\"command\": \"removeCharacterAtIndex\", \"index\": 1}");
+    commandController.runScript(
+    "{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"character\": \"-\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"character\": \"a\", \"command\": \"insertCharacterAtIndex\", \"index\": 0}"
+    "{\"command\": \"removeCharacterAtIndex\", \"index\": 1}");
     EXPECT_EQ("aa", commandController.getCurrentStoredStringValue());
 }
