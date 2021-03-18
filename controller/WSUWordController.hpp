7df519cd761475d4623fc6b92370c01c15d2622f
Erik M. Buck <erik.buck@sbcglobal.net>
2021-03-18 17:37:12 -0400

    Task-12 Added sample of JSON formatted script

--- controller/WSUWordController.hpp
+++ controller/WSUWordController.hpp
@@ -20,8 +20,11 @@ namespace Controller {
 /// This class creates an instance of the Model and serves as a clearing house for operations upon the Model. For example, use runCommandWithUndoRedoSupport() to run commands that mutate the Model in such a way that those mutations may be undone or redone at a later time. Similarly, use runScript() to run an entire script of commands that mutate the Model.
 class WSUWordController {
 private:
-    Model::StoredString::p_t m_storedString_p;  /// An instance of the Model
-    UndoManager m_undoManager;                  /// \ref R2_0 \ref R2_2
+    /// An instance of the Model
+    Model::StoredString::p_t m_storedString_p;
+    
+    /// \ref R2_0 \ref R2_2
+    UndoManager m_undoManager;
     
 public:
     typedef Model::StoredString::command_p_t command_p_t;
@@ -34,7 +37,8 @@ public:
     /// \imp \ref R2_0 \ref R2_2 Use this function to execute commands that mutate the Model associated with this "Supervising Controller" in such a way that the commands may be undone or redone in the future.
     void runCommandWithUndoRedoSupport(command_p_t);
     
-    /// \imp \ref R3_0 Use this function to run all of the commands in a human readable script so that the commands mutate the Model associated with this "Supervising Controller".
+    /// \imp \ref R3_0 Use this function to run all of the commands in a human readable script so that the commands mutate the Model associated with this "Supervising Controller". Scripts are formatted as JSON describing a sequence of commands and the operands for those commands. Example:
+    /// <p> <i>{"command": "insertCharacterAtIndex", "character": "a", "index\": 0} <br> {"command": "insertCharacterAtIndex", "character": "-", "index": 0} <br> {"command": "insertCharacterAtIndex", "character": "a", "index": 0} <br>  {"command": "removeCharacterAtIndex", "index": 1} </i></p>
     void runScript(std::string script);
     
     
